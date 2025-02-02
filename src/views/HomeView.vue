436c885a56e0e05ac47d17b379c71667b73376df
Jakub Hajduk <jakubhajduk53@gmail.com>
2025-02-02 17:25:35 +0100

    feat: add routing

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex flex-col items-center">
+    <div class="text-3xl">Random 10 questions</div>
+    <button class="text-xl border rounded-xl p-1 cursor-pointer">Start</button>
+  </div>
+</template>
+
+<style scoped></style>
