348d444b3d5aa78857cb3ab5573f816046896959
Jakub Hajduk <jakubhajduk53@gmail.com>
2025-05-24 08:36:37 +0200

    feat: update views

--- src/components/QuizElement.vue
+++ src/components/QuizElement.vue
@@ -5,27 +5,30 @@ import { ElRadio, ElRadioGroup } from "element-plus";
 const quizStore = useQuizStore();
 </script>
 <template>
-  <div class="flex flex-col h-full justify-center gap-2 md:w-[35em]">
+  <div class="flex flex-col w-full h-full justify-center">
     <div
       v-html="quizStore.getCurrentQuestionText"
-      class="text-base md:text-xl font-bold overflow-x-auto"
-    ></div>
-    <el-radio-group
-      :model-value="quizStore.userAnswers[quizStore.currentQuestion]"
-      @update:model-value="quizStore.saveAnswer($event as string)"
-      class="flex justify-center"
-      size="large"
-    >
-      <el-radio
-        v-for="answer in quizStore.getQuestions"
-        :key="answer"
-        :value="answer"
-        ><div
-          v-html="answer"
-          class="text-sm md:text-base overflow-x-auto"
-        ></div>
-      </el-radio>
-    </el-radio-group>
+      class="flex justify-center items-center text-base md:text-xl font-bold overflow-x-auto max-w-[95%] min-h-[5ch]"
+    />
+    <hr class="text-[hsl(39,10%,20%)] w-full opacity-10 my-1 md:my-3" />
+    <div class="flex justify-center items-center flex-1">
+      <el-radio-group
+        :model-value="quizStore.userAnswers[quizStore.currentQuestion]"
+        @update:model-value="quizStore.saveAnswer($event as string)"
+        size="large"
+        class="flex flex-col max-w-[90%] gap-1 md:gap-3"
+      >
+        <el-radio
+          v-for="answer in quizStore.getQuestions"
+          :key="answer"
+          :value="answer"
+          ><div
+            v-html="answer"
+            class="text-sm md:text-base overflow-x-auto"
+          ></div>
+        </el-radio>
+      </el-radio-group>
+    </div>
   </div>
 </template>
 
@@ -41,8 +44,19 @@ const quizStore = useQuizStore();
   --el-radio-text-color: hsl(39, 10%, 40%);
   --el-radio-input-border-color-hover: hsl(338, 85%, 46%);
   --el-radio-input-border: 1px solid hsl(39, 10%, 40%);
+  display: flex;
+  gap: 10px;
+}
+
+@media (max-width: 767px) {
+  .el-radio ::v-deep() {
+    gap: 5px;
+  }
 }
 .el-radio ::v-deep(.el-radio__inner) {
   background-color: hsl(39, 50%, 95%);
+}
+.el-radio-group {
+  align-items: start;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -16,7 +16,7 @@ onMounted(() => {
 
 <template>
   <div
-    class="flex flex-col justify-center items-center text-center w-[600px] gap-5 md:gap-10 m-3 md:m-5 px-5 py-10 rounded-sm bg-radial from-gray-tertiary to-gray-secondary shadow-[0px_3px_5px_hsla(39,15%,50%,50%)] inset-shadow-[0px_3px_0px_hsl(39,30%,90%)]"
+    class="flex flex-col justify-center items-center text-center w-[600px] gap-5 md:gap-10 m-3 md:m-5 px-5 py-10 rounded-sm bg-radial from-gray-tertiary to-gray-secondary shadow-[-3px_3px_0px_hsla(39,20%,25%,10%)] inset-shadow-[-3px_3px_0px_hsla(39,20%,95%,10%)]"
   >
     <div class="flex flex-col items-center gap-1 md:gap-3">
       <div class="text-xl md:text-3xl font-bold">Random 10 questions</div>
